43e579bcf1d70e88c76e8d7556f74e1fc205ed66
Lateefat Amuda <teepha06@gmail.com>
2020-03-09 15:52:55 +0100

    -  setup project - configure database

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  resources :tickets do
+    member do
+      get :update_status
+    end
+    resources :comments, except: [:edit, :update, :new]
+  end
+end

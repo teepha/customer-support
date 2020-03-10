2934ce9a496fc5ffb068bcae16cb3c78c8fdac5f
Lateefat Amuda <teepha06@gmail.com>
2020-03-10 08:42:52 +0100

    - install rubyzip gem for zipping and reading zip files - implement agent can fetch and export closed reports within the last one month

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -7,8 +7,12 @@ class TicketsController < ApplicationController
     unless current_user.is_agent? || current_user.is_admin?
       return @tickets = current_user.tickets.order('created_at DESC')
     end
-
     @tickets = Ticket.all.order('created_at DESC')
+  end
+
+  def closed_tickets
+    @tickets = Ticket.tickets_closed_within_a_month
+    get_zip_data(@tickets)
   end
 
   def new
@@ -56,5 +60,26 @@ class TicketsController < ApplicationController
     @ticket = Ticket.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     flash[:error] = 'Sorry, record not found!'
+  end
+
+  def create_zip_folder(folder_name, report_data)
+    file_name = "#{folder_name}.zip"
+    ::Zip::File.open(file_name, Zip::File::CREATE) do |zipfile|
+      report_data.each do |csv_file_name, report|
+        zipfile.get_output_stream("#{csv_file_name}.csv") do |file|
+          file.write report
+        end
+      end
+    end
+    file_name
+  end
+
+  def get_zip_data(data)
+    file_name = create_zip_folder("tickets_report_csv", data)
+    File.open(file_name, "r") do |file|
+      send_data(file.read, type: "application/zip", filename: file_name,
+                           disposition: "attachment")
+    end
+    File.delete(file_name)
   end
 end
